b650d94c96d0da6d0e3c0b0aa74f5fd7b7379603
Abhijeet Kasurde <akasurde@redhat.com>
2022-09-19 17:19:18 +0530

    Update inventory files
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import os
-import json
-from unittest import removeHandler
-import ansible_runner
 import asyncio
+import configparser
+import json
+import os
+import sys
 import tempfile
 import yaml
-import configparser
+import ansible_runner
 
 from ansible_sdk import AnsibleJobDef
 from ansible_sdk.executors import AnsibleSubprocessJobExecutor
@@ -23,10 +23,48 @@ app = Flask(
 
 
 @app.route("/")
-def index():
-    filename = os.path.join(app.instance_path, "inventory/gcp_compute.yml")
-    hosts = get_inventory(filename=filename)
-    return render_template("index.html", hosts=hosts)
+async def index():
+    inventory_type = app.config['inventory_type']
+    filename = os.path.join(app.instance_path, "inventory/%s.yml" % inventory_type)
+    datadir_path = create_temp_dir()
+    print("INFO: Gathering information about inventory from %s" % inventory_type)
+    with open(filename, 'r') as fh:
+        config = yaml.safe_load(fh.read())
+
+    if inventory_type == 'gcp_compute':
+        app.config['service_account_file'] = config.get('service_account_file')
+        inventory_data = {
+            'zone': config.get('zones')[0],
+            'project': config.get('projects'),
+            'service_file': app.config['service_account_file'],
+        }
+        create_gcp_inventory_playbook(
+            datadir_path=datadir_path,
+            **inventory_data
+        )
+    elif inventory_type == 'azure':
+        inventory_data = {}
+    else:
+        print("ERR: Unable to parse inventory")
+        return render_template(
+            "index.html",
+            message={
+                "error": "Unable to parse inventory file provided"
+            }
+        )
+
+    d = await run_playbook(datadir_path=datadir_path)
+    print("Return from Ansible SDK %s" % d)
+
+    return render_template(
+        "index.html",
+        hosts=d,
+        zone=config.get('zones')[0],
+        project=config.get('projects'),
+        message={
+            "success": "True"
+        }
+    )
 
 
 @app.route("/powerstate_host", methods=["POST"])
@@ -43,12 +81,13 @@ async def manage_powerstate_host():
             'machine_type': data['machine_type'],
             'zone': data['zone'],
             'project': data['project'],
-            'service_file': '/Users/akasurde/.gcp_auth',
+            'service_file': app.config['service_account_file'],
         }
     )
     d = await run_playbook(datadir_path=datadir_path)
     print("Return from Ansible SDK %s" % d)
-    return {"ipAddress": data["ipAddress"], "response": d.res, "success": True}
+    return {"ipAddress": data["ipAddress"], "response": d, "success": True}
+
 
 @app.route("/ping_host", methods=["POST"])
 async def ping_host():
@@ -58,7 +97,7 @@ async def ping_host():
     create_ping_playbook(datadir_path=datadir_path)
     d = await run_playbook(datadir_path=datadir_path)
     print("Return from Ansible SDK %s" % d)
-    return {"ipAddress": data["ipAddress"], "response": d.res, "success": True}
+    return {"ipAddress": data["ipAddress"], "response": d, "success": True}
 
 
 def create_temp_dir():
@@ -69,12 +108,12 @@ def create_temp_dir():
 
     return datadir_path
 
-def create_powerstate_playbook(datadir_path, **kwargs):
 
+def create_powerstate_playbook(datadir_path, **kwargs):
     if kwargs['desired_powerstate'] == 'poweredoff':
-        tp_filename = 'powerstate_off.yml'
+        tp_filename = 'gcp_powerstate_off.yml'
     else:
-        tp_filename = 'powerstate_on.yml'
+        tp_filename = 'gcp_powerstate_on.yml'
     powerstate_tp = render_template(
         tp_filename,
         machine_name=kwargs['machine_name'],
@@ -82,14 +121,28 @@ def create_powerstate_playbook(datadir_path, **kwargs):
         zone=kwargs['zone'],
         project=kwargs['project'],
         service_file=kwargs['service_file'],
-        )
+    )
+    with open(os.path.join(datadir_path, "project", "pb.yml"), "w") as fh:
+        fh.write(powerstate_tp)
+
+
+def create_gcp_inventory_playbook(datadir_path, **kwargs):
+    tp_filename = 'gcp_instance_info.yml'
+    instance_info_tp = render_template(
+        tp_filename,
+        zone=kwargs['zone'],
+        project=kwargs['project'],
+        service_file=kwargs['service_file'],
+    )
     with open(os.path.join(datadir_path, "project", "pb.yml"), "w") as f:
-        f.write(powerstate_tp)
+        f.write(instance_info_tp)
+
 
 def create_ping_playbook(datadir_path):
     ping_tp = render_template('ping.yml')
     with open(os.path.join(datadir_path, "project", "pb.yml"), "w") as f:
         f.write(ping_tp)
+
 
 def create_inventory(datadir_path, host):
     inventory = configparser.ConfigParser(allow_no_value=True)
@@ -100,6 +153,7 @@ def create_inventory(datadir_path, host):
     with open(os.path.join(datadir_path, "inventory", "hosts"), "w") as f:
         inventory.write(f)
 
+
 async def run_playbook(datadir_path):
     executor = AnsibleSubprocessJobExecutor()
 
@@ -107,18 +161,14 @@ async def run_playbook(datadir_path):
 
     job_status = await executor.submit_job(jobdef)
 
-    # consume events and accumulate stdout replica
-    stdout = ""
-
     # consume events as they arrive
-    eventcount = 0
     result = {"res": {}}
     async for ev in job_status.events:
-        eventcount += 1
-        if isinstance(ev, RunnerOnOKEvent):
-            result = ev
+        # print(ev)
+        if isinstance(ev, RunnerOnOKEvent) and ev.event_data.task_action == 'debug':
+            result = ev.event_data.res
     print("Result from Ansible SDK %s" % result)
-    return result.event_data or {}
+    return result or {}
 
 
 def get_inventory(filename=None):
@@ -133,4 +183,9 @@ def get_inventory(filename=None):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        sys.exit("Please specify inventory type (valid values: 'gcp_compute', 'azure')")
+    inventory_type = sys.argv[1]
+
+    app.config['inventory_type'] = inventory_type
     app.run(debug=True)
